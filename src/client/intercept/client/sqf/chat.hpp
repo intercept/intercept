a732cb7c861ad23c48a3b71fb9ad255daa210981
Nou <cliff.foster@idi-systems.com>
2015-12-04 17:50:43 -0800

    Headers corrected.

--- src/client/intercept/client/sqf/chat.hpp
+++ src/client/intercept/client/sqf/chat.hpp
@@ -7,7 +7,13 @@ using namespace intercept::client::types;
 
 namespace intercept {
     namespace sqf {
-        float random(float max_);
+        namespace __helpers {
+            void chat_message(binary_function fnc_, object obj_, const std::string &message_);
+        }
         void side_chat(object obj_, const std::string &message_);
+        void global_chat(object obj_, const std::string & message_);
+        void group_chat(object obj_, const std::string & message_);
+        void vehicle_chat(object obj_, const std::string & message_);
+        void command_chat(object obj_, const std::string & message_);
     }
 }

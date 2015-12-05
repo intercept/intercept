8572c9a20e43a362ce79380c6ab0166f2da456bf
Verox <verox.averre@gmail.com>
2015-12-05 22:12:22 +0000

    Implemented common unary helpers.

--- src/client/intercept/client/sqf/common_helpers.hpp
+++ src/client/intercept/client/sqf/common_helpers.hpp
@@ -24,20 +24,38 @@ namespace intercept {
             config __retrieve_nular_config(nular_function fnc_);
             display __retrieve_nular_display(nular_function fnc_);
 
+			std::vector<team_member> _nular_team_members(nular_function fnc_);
+			std::vector<std::string> _nular_strings(nular_function fnc_);
+			std::vector<object> all_objects(nular_function fnc_);
+			std::vector<group> all_groups(nular_function fnc_);
+			std::vector<display> all_displays(nular_function fnc_);
+			std::vector<marker> all_map_markers(nular_function fnc_);
+
 			/* Unary function helpers */
 			/** Void return, standard input helpers **/
 			void __empty_unary_obj(unary_function fnc_, object obj_);
-			void __empty_unary_vector(unary_function fnc_, vector3 obj_);
+			void __empty_unary_vector(unary_function fnc_, vector3 vec_);
 			void __empty_unary_string(unary_function fnc_, std::string str_);
 			void __empty_unary_number(unary_function fnc_, float val_);
 			void __empty_unary_bool(unary_function fnc_, bool val_);
+			void __empty_unary_control(unary_function fnc_, control ctl_);
 
-            std::vector<team_member>& _nular_team_members(nular_function fnc_);
-            std::vector<std::string>& _nular_strings(nular_function fnc_);
-            std::vector<object>& all_objects(nular_function fnc_);
-            std::vector<group>& all_groups(nular_function fnc_);
-            std::vector<display>& all_displays(nular_function fnc_);
-            std::vector<marker>& all_map_markers(nular_function fnc_);            
+			/** Common return/input helpers **/
+			bool __bool_unary_object(unary_function fnc_, object obj_);
+			bool __bool_unary_string(unary_function fnc_, std::string str_);
+			bool __bool_unary_float(unary_function fnc_, float val_);
+			bool __bool_unary_control(unary_function fnc_, control ctl_);
+
+			float __number_unary_float(unary_function fnc_, float val_);
+			float __number_unary_control(unary_function fnc_, control ctl_);
+			float __number_unary_string(unary_function fnc_, std::string str_);
+			float __number_unary_object(unary_function fnc_, object obj_);
+
+			std::string __string_unary_control(unary_function fnc_, control ctl_);
+			std::string __string_unary_object(unary_function fnc_, object obj_);
+			std::string __string_unary_string(unary_function fnc_, std::string str_);
+
+			object __object_unary_object(unary_function fnc_, object obj_);
 		}
 	}
 }

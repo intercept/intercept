1972425728321da37c580f26a3d8aef20a85b658
Glowbal <thomasskooi@live.nl>
2015-12-05 17:11:56 +0100

    Add wrappers for all* sqf functions

--- src/client/intercept/client/sqf/core.cpp
+++ src/client/intercept/client/sqf/core.cpp
@@ -4,6 +4,56 @@
 
 namespace intercept {
     namespace sqf {
+        namespace __helpers {
+            
+            std::vector<object>& all_objects(nular_function fnc_) {
+                game_value intersects_value = host::functions.invoke_raw_nular(fnc_);
+                game_data_array* intersects = ((game_data_array *)intersects_value.data);
+
+                std::vector<object> output;
+                for (int i = 0; i < intersects->length; ++i) {
+                    output.push_back(std::make_shared<object_ptr>(intersects->data[i]));
+                }
+                host::functions.free_value(&intersects_value);
+                return output;
+            }
+
+            std::vector<group>& all_groups(nular_function fnc_) {
+                game_value intersects_value = host::functions.invoke_raw_nular(fnc_);
+                game_data_array* intersects = ((game_data_array *)intersects_value.data);
+
+                std::vector<group> output;
+                for (int i = 0; i < intersects->length; ++i) {
+                    output.push_back(std::make_shared<group_ptr>(intersects->data[i]));
+                }
+                host::functions.free_value(&intersects_value);
+                return output;
+            }
+
+            std::vector<display>& all_displays(nular_function fnc_) {
+                game_value intersects_value = host::functions.invoke_raw_nular(fnc_);
+                game_data_array* intersects = ((game_data_array *)intersects_value.data);
+
+                std::vector<display> output;
+                for (int i = 0; i < intersects->length; ++i) {
+                    output.push_back(std::make_shared<display_ptr>(intersects->data[i]));
+                }
+                host::functions.free_value(&intersects_value);
+                return output;
+            }
+            std::vector<marker>& all_map_markers(nular_function fnc_) {
+                game_value intersects_value = host::functions.invoke_raw_nular(fnc_);
+                game_data_array* intersects = ((game_data_array *)intersects_value.data);
+
+                std::vector<marker> output;
+                for (int i = 0; i < intersects->length; ++i) { // TODO figure out converting markers in an array to an std vector marker 
+                    // output.push_back((host::functions.new_string((const char *)(intersects->data[i]))));
+                }
+                host::functions.free_value(&intersects_value);
+                return output;
+            }
+        }
+
         object player() {
             game_value player_obj = host::functions.invoke_raw_nular(client::__sqf::nular__player__ret__object);
             return std::make_shared<object_ptr>(player_obj);
@@ -33,6 +83,50 @@ namespace intercept {
         void delete_vehicle(object obj_)
         {
             host::functions.invoke_raw_unary(__sqf::unary__deletevehicle__object__ret__nothing, *obj_);
+        }
+
+        std::vector<object>& all_3den_entities() {
+            return __helpers::all_objects(client::__sqf::nular__all3denentities__ret__array);
+        }
+
+        std::vector<object>& all_curators() {
+            return __helpers::all_objects(client::__sqf::nular__all3denentities__ret__array);
+        }
+
+        std::vector<object>& all_dead() {
+            return __helpers::all_objects(client::__sqf::nular__alldead__ret__array);
+        }
+
+        std::vector<object>& all_deadmen() {
+            return __helpers::all_objects(client::__sqf::nular__alldeadmen__ret__array);
+        }
+
+        std::vector<display>& all_displays() {
+            return __helpers::all_displays(client::__sqf::nular__alldisplays__ret__array);
+        }
+
+        std::vector<group>& all_groups() {
+            return __helpers::all_groups(client::__sqf::nular__allgroups__ret__array);
+        }
+
+        std::vector<marker>& all_map_markers() {
+            return __helpers::all_map_markers(client::__sqf::nular__allmapmarkers__ret__array);
+        }
+
+        std::vector<object>& all_mines() {
+            return __helpers::all_objects(client::__sqf::nular__allmines__ret__array);
+        }
+
+        std::vector<object>& all_players() {
+            return __helpers::all_objects(client::__sqf::nular__allplayers__ret__array);
+        }
+
+        std::vector<object>& all_units() {
+            return __helpers::all_objects(client::__sqf::nular__allunits__ret__array);
+        }
+
+        std::vector<object>& all_units_uav() {
+            return __helpers::all_objects(client::__sqf::nular__allunitsuav__ret__array);
         }
     }
 }

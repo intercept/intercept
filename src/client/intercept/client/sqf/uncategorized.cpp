a3b3dbfe3675ff6a4df9ea709f2eb99c9ab73af5
Verox <verox.averre@gmail.com>
2015-12-06 23:20:38 +0000

    Adds action_keys*, add_cam_shake and add_*_pool wrappers.

---
+++ src/client/intercept/client/sqf/uncategorized.cpp
@@ -0,0 +1,133 @@
+#include "uncategorized.hpp"
+#include "client\pointers.hpp"
+#include "common_helpers.hpp"
+
+
+using namespace intercept::client::types;
+
+namespace intercept {
+	namespace sqf {
+		namespace __uncategorized {
+			std::vector<std::string> action_keys(std::string user_action_)
+			{
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeys__string__ret__array, game_value_string(user_action_));
+				std::vector<std::string> r_arr = __helpers::__convert_to_strings_vector(act_keys);
+
+				host::functions.free_value(&act_keys);
+				return r_arr;
+			}
+
+
+			std::string action_keys_names(std::string user_action_)
+			{
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeysnames__string_array__ret__string, game_value_string(user_action_));
+				std::string r_str = ((game_data_string *)act_keys.data)->get_string();
+
+				host::functions.free_value(&act_keys);
+				return r_str;
+			}
+
+			std::string action_keys_names(std::string user_action_, float max_keys_)
+			{
+				game_value_array<2> params({
+					game_value_string(user_action_),
+					game_value_number(max_keys_)
+				});
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeysnames__string_array__ret__string, params);
+				std::string r_str = ((game_data_string *)act_keys.data)->get_string();
+
+				host::functions.free_value(&act_keys);
+				return r_str;
+			}
+			std::string action_keys_names(std::string user_action_, float max_keys_, std::string input_device_priority_)
+			{
+				game_value_array<3> params({
+					game_value_string(user_action_),
+					game_value_number(max_keys_),
+					game_value_string(input_device_priority_)
+				});
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeysnames__string_array__ret__string, params);
+				std::string r_str = ((game_data_string *)act_keys.data)->get_string();
+
+				host::functions.free_value(&act_keys);
+				return r_str;
+			}
+
+			std::vector<std::string> action_keys_names_array(std::string user_action_)
+			{
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeysnamesarray__string_array__ret__array, game_value_string(user_action_));
+				std::vector<std::string> r_arr = __helpers::__convert_to_strings_vector(act_keys);
+
+				host::functions.free_value(&act_keys);
+				return r_arr;
+			}
+
+			std::vector<std::string> action_keys_names_array(std::string user_action_, float max_keys_)
+			{
+				game_value_array<2> params({
+					game_value_string(user_action_),
+					game_value_number(max_keys_)
+				});
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeysnamesarray__string_array__ret__array, params);
+				std::vector<std::string> r_arr = __helpers::__convert_to_strings_vector(act_keys);
+
+				host::functions.free_value(&act_keys);
+				return r_arr;
+			}
+			std::vector<std::string> action_keys_names_array(std::string user_action_, float max_keys_, std::string input_device_priority_)
+			{
+				game_value_array<3> params({
+					game_value_string(user_action_),
+					game_value_number(max_keys_),
+					game_value_string(input_device_priority_)
+				});
+
+				game_value act_keys = host::functions.invoke_raw_unary(client::__sqf::unary__actionkeysnamesarray__string_array__ret__array, params);
+				std::vector<std::string> r_arr = __helpers::__convert_to_strings_vector(act_keys);
+
+				host::functions.free_value(&act_keys);
+				return r_arr;
+			}
+
+			void activate_addons(std::vector<std::string> &addons_)
+			{
+				game_value_array_dynamic<std::string, game_value_string> addons_arr(addons_);
+
+				host::functions.invoke_raw_unary(client::__sqf::unary__activateaddons__array__ret__nothing, addons_arr);
+			}
+
+			/* potential namespace: camera */
+			void add_cam_shake(float power_, float duration_, float frequency_)
+			{
+				game_value_array<3> params({
+					game_value_number(power_),
+					game_value_number(duration_),
+					game_value_number(frequency_)
+				});
+
+				host::functions.invoke_raw_unary(client::__sqf::unary__addcamshake__array__ret__nothing, params);
+			}
+
+			/* potential namespace: items, inventory, campaign */
+			void add_item_pool(std::string item_name_, float item_count_)
+			{
+				game_value_array<3> params({
+					game_value_string(item_name_),
+					game_value_number(item_count_)
+				});
+
+				host::functions.invoke_raw_unary(client::__sqf::unary__additempool__array__ret__nothing, params);
+			}
+
+			void add_magazine_pool(std::string mag_name_, float mag_count_)
+			{
+				game_value_array<3> params({
+					game_value_string(mag_name_),
+					game_value_number(mag_count_)
+				});
+
+				host::functions.invoke_raw_unary(client::__sqf::unary__additempool__array__ret__nothing, params);
+			}
+		}
+	}
+}
---
+++ src/client/intercept/client/sqf/uncategorized.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "shared.hpp"
+#include "client\client.hpp"
+#include "client\client_types.hpp"
+
+using namespace intercept::client::types;
+
+namespace intercept {
+	namespace sqf {
+		namespace __uncategorized {
+			/* potential namespace: */
+			std::vector<std::string> action_keys(std::string user_action_);
+
+			std::string action_keys_names(std::string user_action_);
+			std::string action_keys_names(std::string user_action_, float max_keys_);
+			std::string action_keys_names(std::string user_action_, float max_keys_, std::string input_device_priority_);
+
+			std::vector<std::string> action_keys_names_array(std::string user_action_);
+			std::vector<std::string> action_keys_names_array(std::string user_action_, float max_keys_);
+			std::vector<std::string> action_keys_names_array(std::string user_action_, float max_keys_, std::string input_device_priority_);
+
+			/* potential namespace: misc, missions */
+			void activate_addons(std::vector<std::string> &addons_);
+
+			/* potential namespace: camera */
+			void add_cam_shake(float power_, float duration_, float frequency_);
+
+			/* potential namespace: items, inventory, campaign */
+			void add_item_pool(std::string item_name_, float item_count_);
+			void add_magazine_pool(std::string mag_name_, float mag_count_);
+		}
+	}
+}

33290e97e6cc11084420e1fa2e0c8b2e1b53d5af
Nou <cliff.foster@idi-systems.com>
2015-12-06 16:19:46 -0800

    Types now wrapping a shared pointer.

--- src/client/intercept/client/client_types.cpp
+++ src/client/intercept/client/client_types.cpp
@@ -5,70 +5,7 @@
 namespace intercept {
     namespace client {
         namespace types {
-            internal_ptr::internal_ptr()
-            {
-            }
-
-            internal_ptr::internal_ptr(rv_types::game_value value_) : value(value_)
-            {
-            }
-
-            internal_ptr::~internal_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            object_ptr::~object_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            group_ptr::~group_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            code_ptr::~code_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            config_ptr::~config_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            control_ptr::~control_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            display_ptr::~display_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            location_ptr::~location_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            script_ptr::~script_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            side_ptr::~side_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            text_ptr::~text_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            team_member_ptr::~team_member_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            rv_namespace_ptr::~rv_namespace_ptr()
-            {
-                host::functions.free_value(&value);
-            }
-            task_ptr::~task_ptr()
-            {
-                host::functions.free_value(&value);
-            }
+            
         }
     }
 }

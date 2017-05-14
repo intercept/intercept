9c851baea0e4d8e0519bf9ea08a7acd2001bfea6
Molaron <Anton.Kurth@gmx.de>
2017-05-14 14:55:29 +0200

    FIX: Wrong binary function for do_watch alternative synta

--- src/client/intercept/client/sqf/uncategorized.cpp
+++ src/client/intercept/client/sqf/uncategorized.cpp
@@ -6472,13 +6472,13 @@ void draw_line_3d(const vector3 & pos1_, const vector3 & pos2_, const rv_color &
         }
 
         void do_watch(const object &unit_, const object &target_) {
-            host::functions.invoke_raw_binary(__sqf::binary__dowatch__object_array__array__ret__nothing, unit_, target_);
+            host::functions.invoke_raw_binary(__sqf::binary__dowatch__object_array__object__ret__nothing, unit_, target_);
         }
 
         void do_watch(const std::vector<object> &units_, const object &target_) {
             auto_array<game_value> units(units_.begin(), units_.end());
 
-            host::functions.invoke_raw_binary(__sqf::binary__dowatch__object_array__array__ret__nothing, std::move(units), target_);
+            host::functions.invoke_raw_binary(__sqf::binary__dowatch__object_array__object__ret__nothing, std::move(units), target_);
         }
     }
 }

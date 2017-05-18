5b94a89caafc86e3c909c9014517397305cae4f0
Molaron <Anton.Kurth@gmx.de>
2017-05-18 12:33:56 +0200

    ADD: binary__ppeffectadjust__scalar__array__ret__nothing

--- src/client/intercept/client/sqf/uncategorized.cpp
+++ src/client/intercept/client/sqf/uncategorized.cpp
@@ -7430,14 +7430,12 @@ void draw_line_3d(const vector3 & pos1_, const vector3 & pos2_, const rv_color &
 
         //#TODO: Replace &settings_ with the right pp_effect_parameters
         void pp_effect_adjust(std::variant<std::reference_wrapper<const std::string>, std::reference_wrapper<int>> effect_, const game_value &settings_) {
-            game_value param_left;
+
             switch (effect_.index())
             {
-            case 0: param_left = std::get<0>(effect_).get();
-            case 1: param_left = static_cast<float>(std::get<1>(effect_).get());
+            case 0: host::functions.invoke_raw_binary(__sqf::binary__ppeffectadjust__string__array__ret__nothing, std::get<0>(effect_).get(), settings_);
+            case 1: host::functions.invoke_raw_binary(__sqf::binary__ppeffectadjust__scalar__array__ret__nothing, static_cast<float>(std::get<1>(effect_).get()), settings_);
             }
-
-            host::functions.invoke_raw_binary(__sqf::binary__ppeffectadjust__string__array__ret__nothing, std::move(param_left), settings_);
         }
     }
 }

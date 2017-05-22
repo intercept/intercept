f419d9157fdd9956e6bbfdc71367cb593f40eed9
dedmen <dedmen@dedmen.de>
2017-05-22 10:39:39 +0200

    Yet another RSQF fix

--- src/host/invoker/invoker.cpp
+++ src/host/invoker/invoker.cpp
@@ -48,10 +48,10 @@ namespace intercept {
     bool invoker::invoker_begin_register(const arguments & args_, std::string & result_) {
 
         sqf_functions::get().initialize();
-        _intercept_event_function = sqf_functions::get().registerFunction("interceptEvent", "", userFunctionWrapper<_intercept_event>, types::__internal::GameDataType::BOOL, types::__internal::GameDataType::ARRAY);
-        _intercept_do_invoke_period_function = sqf_functions::get().registerFunction("interceptOnFrame", "", userFunctionWrapper<_intercept_do_invoke_period>, types::__internal::GameDataType::BOOL, types::__internal::GameDataType::ARRAY);
-        _intercept_signal_function = sqf_functions::get().registerFunction("interceptSignal", "", userFunctionWrapper<_intercept_signal>, types::__internal::GameDataType::BOOL, types::__internal::GameDataType::ARRAY);
         _intercept_registerTypes_function = sqf_functions::get().registerFunction("interceptRegisterTypes", "", userFunctionWrapper<_intercept_registerTypes>, types::__internal::GameDataType::BOOL, types::__internal::GameDataType::NAMESPACE);
+        _intercept_event_function = sqf_functions::get().registerFunction("interceptEvent", "", userFunctionWrapper<_intercept_event>, types::__internal::GameDataType::BOOL, types::__internal::GameDataType::STRING, types::__internal::GameDataType::ARRAY);
+        _intercept_do_invoke_period_function = sqf_functions::get().registerFunction("interceptOnFrame", "", userFunctionWrapper<_intercept_do_invoke_period>, types::__internal::GameDataType::BOOL);
+        _intercept_signal_function = sqf_functions::get().registerFunction("interceptSignal", "", userFunctionWrapper<_intercept_signal>, types::__internal::GameDataType::BOOL, types::__internal::GameDataType::ARRAY, types::__internal::GameDataType::ARRAY);
         //#deprecate
         //if (loader::get().hook_function("str", _register_hook, _register_hook_trampoline)) {
         //    LOG(INFO) << "Registration function hooked.";
@@ -82,7 +82,7 @@ namespace intercept {
         return true;
     }
 
-    game_value invoker::_intercept_do_invoke_period(game_value right_arg_) {
+    game_value invoker::_intercept_do_invoke_period() {
         return invoker::get().do_invoke_period();
     }
 
@@ -104,8 +104,8 @@ namespace intercept {
         return true;
     }
 
-    game_value invoker::_intercept_event(game_value left_arg_) {
-        return invoker::get().rv_event(left_arg_[0], left_arg_[1]);
+    game_value invoker::_intercept_event(game_value left_arg_, game_value right_arg_) {
+        return invoker::get().rv_event(left_arg_, right_arg_);
     }
 
     bool invoker::rv_event(const std::string& event_name_, game_value& params_) {
@@ -127,8 +127,8 @@ namespace intercept {
         return false;
     }
 
-    game_value invoker::_intercept_signal(game_value left_arg_) {
-        return invoker::get().signal(left_arg_[0], left_arg_[1], left_arg_[2]);
+    game_value invoker::_intercept_signal(game_value left_arg_, game_value right_arg_) {
+        return invoker::get().signal(left_arg_[0], left_arg_[1], right_arg_);
     }
 
     bool invoker::signal(const std::string& extension_name, const std::string& signal_name, game_value args) {

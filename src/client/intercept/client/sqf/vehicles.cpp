604df84cdbc7d8f5a5e0b923d0950080a890e6b1
RZSenfo <lpsenfo@web.de>
2017-10-31 20:12:33 +0100

    fix getallhitpointsdamage crash on null objects (#166)
    * fix crash on null objects
    * implement startup parameters:
    -interceptDebugLog (enables DEBUG level for easylogging)
    -interceptLogFile "path" (creates logfile at given path, path needs to be enclosed in quotation marks)

--- src/client/intercept/client/sqf/vehicles.cpp
+++ src/client/intercept/client/sqf/vehicles.cpp
@@ -600,11 +600,7 @@ namespace intercept {
         rv_hit_points_damage get_all_hit_points_damage(const object &veh_) {
             game_value ret = host::functions.invoke_raw_unary(__sqf::unary__getallhitpointsdamage__object__ret__array, veh_);
 
-            sqf_return_string_list hit_points = __helpers::__convert_to_strings_vector(ret[0]);
-            sqf_return_string_list hit_selections = __helpers::__convert_to_strings_vector(ret[1]);
-            std::vector<float> damages = __helpers::__convert_to_numbers_vector(ret[2]);
-
-            return rv_hit_points_damage({hit_points, hit_selections, damages});
+            return rv_hit_points_damage(ret);
         }
 
         sqf_return_string_list get_object_materials(const object &object_) {

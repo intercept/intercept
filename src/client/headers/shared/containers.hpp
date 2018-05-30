48af818a2cbb3da88090eefc4431689d71ae01e0
Dedmen Miller <dedmen@dedmen.de>
2018-05-30 14:11:12 +0200

    add i_ref

--- src/client/headers/shared/containers.hpp
+++ src/client/headers/shared/containers.hpp
@@ -243,6 +243,8 @@ namespace intercept::types {
         static_assert(std::is_base_of<refcount_base, Type>::value, "Type must inherit refcount_base");
         Type* _ref{ nullptr };
     public:
+        using baseType = Type;
+
         constexpr ref() noexcept = default;
         ~ref() { free(); }
 
@@ -297,6 +299,65 @@ namespace intercept::types {
         size_t ref_count() const noexcept { return _ref ? _ref->ref_count() : 0; }
     };
 
+    //specialization for I_debug_value. You should not be using this.
+    template <class Type>
+    class i_ref {
+        Type* _ref{nullptr};
+    public:
+        using baseType = Type;
+        constexpr i_ref() noexcept = default;
+        ~i_ref() { free(); }
+
+        //Construct from Pointer
+        constexpr i_ref(Type* other_) noexcept {
+            if (other_) other_->IaddRef();
+            _ref = other_;
+        }
+        //Copy from pointer
+        i_ref& operator=(Type* source_) noexcept {
+            Type* old = _ref;
+            if (source_) source_->IaddRef();
+            _ref = source_;
+            if (old) old->release();  //decrement reference and delete object if refcount == 0
+            return *this;
+        }
+
+        //Construct from reference
+        constexpr i_ref(const i_ref& source_ref_) noexcept {
+            Type* source = source_ref_._ref;
+            if (source) source->IaddRef();
+            _ref = source;
+        }
+        //Copy from reference.
+        i_ref& operator=(const i_ref& other_) {
+            Type* source = other_._ref;
+            Type* old = _ref;
+            if (source) source->IaddRef();
+            _ref = source;
+            if (old) old->Irelease();  //decrement reference and delete object if refcount == 0
+            return *this;
+        }
+        void swap(i_ref& other_) noexcept {
+            auto temp = other_._ref;
+            other_._ref = _ref;
+            _ref = temp;
+        }
+        constexpr bool is_null() const noexcept { return _ref == nullptr; }
+        void free() noexcept {
+            if (!_ref) return;
+            _ref->Irelease();
+            _ref = nullptr;
+        }
+        //This returns a pointer to the underlying object. Use with caution!
+
+        [[deprecated]] constexpr Type* getRef() const noexcept { return _ref; }
+        ///This returns a pointer to the underlying object. Use with caution!
+        constexpr Type* get() const noexcept { return _ref; }
+        constexpr Type* operator->() const noexcept { return _ref; }
+        operator Type*() const noexcept { return _ref; }
+        bool operator!=(const ref<Type>& other_) const noexcept { return _ref != other_._ref; }
+        size_t ref_count() const noexcept { return _ref ? _ref->ref_count() : 0; }
+    };
 
     ///When this goes out of scope. The pointer is deleted. This takes ownership of the pointer
     template<class Type>

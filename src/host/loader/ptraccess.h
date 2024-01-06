7a32180ad41190f5bd79939e185d76e0f4452566
Leo <16115820+Leopard20@users.noreply.github.com>
2024-01-06 21:17:05 +0330

    Loader fixes and improvements (#291)
    * Use better and safer alternative to IsBadReadPtr;
    Fix loader for vc143;
    Fix hashmap poolAlloc
    
    * Fix Linux build

--- src/host/loader/ptraccess.h
+++ src/host/loader/ptraccess.h
@@ -2,6 +2,46 @@
 #pragma once
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
+#ifdef __cplusplus
+// From: https://stackoverflow.com/questions/496034/most-efficient-replacement-for-isbadreadptr
+// Check memory address access
+static const DWORD dwForbiddenArea = PAGE_GUARD | PAGE_NOACCESS;
+static const DWORD dwReadRights = PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY;
+static const DWORD dwWriteRights = PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY;
+
+template<DWORD dwAccessRights>
+bool CheckAccess(const void* pAddress, size_t nSize)
+{
+    if (!pAddress || !nSize)
+    {
+        return false;
+    }
+
+    MEMORY_BASIC_INFORMATION sMBI;
+    bool bRet = false;
+
+    UINT_PTR pCurrentAddress = UINT_PTR(pAddress);
+    UINT_PTR pEndAdress = pCurrentAddress + (nSize - 1);
+
+    do
+    {
+        ZeroMemory(&sMBI, sizeof(sMBI));
+        VirtualQuery(LPCVOID(pCurrentAddress), &sMBI, sizeof(sMBI));
+
+        bRet = (sMBI.State & MEM_COMMIT) // memory allocated and
+            && !(sMBI.Protect & dwForbiddenArea) // access to page allowed and
+            && (sMBI.Protect & dwAccessRights); // the required rights
+
+        pCurrentAddress = (UINT_PTR(sMBI.BaseAddress) + sMBI.RegionSize);
+    } while (bRet && pCurrentAddress <= pEndAdress);
+
+    return bRet;
+}
+
+#define IsBadWritePtr(p,n) (!CheckAccess<dwWriteRights>(p,n))
+#define IsBadReadPtr(p,n) (!CheckAccess<dwReadRights>(p,n))
+#define IsBadStringPtrW(p,n) (!CheckAccess<dwReadRights>(p,n*2))
+#endif /* defined(__cplusplus) */
 #else
 #define __CUSTOM_ISBADREADPTR
 #ifdef __cplusplus
